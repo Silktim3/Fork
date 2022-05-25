0e065f9e1cee21efee3fb3f23baed570c8a8fad1
jctest <k120v4@protonmail.com>
2022-05-25 11:58:20 -0700

    started=1

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ export const config = {
   contractAddress: "0xe962Ea5AB454b280A82A0FDc1cd43DcBb6A4021c", 
   scanLink:
     "https://ftmscan.com/address/0xe962Ea5AB454b280A82A0FDc1cd43DcBb6A4021c",
-  started: 0
+  started: 1
 };
